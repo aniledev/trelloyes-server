236e9dea46ba0b85b0cba0924ce21d08d58d9ce8
aniledev <elinamcgill@gmail.com>
2021-01-05 12:30:47 -0500

    CHORE: add and configure the winston logger

---
+++ src/app.js
@@ -0,0 +1,53 @@
+// IMPORT REQUIRED LIBRARIES AND SECURITY PACKAGES
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const winston = require("winston");
+``;
+const { NODE_ENV } = require("./config");
+
+const app = express();
+
+const morganOption = NODE_ENV === "production" ? "tiny" : "dev";
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  transports: [new winston.transports.File({ filename: "info.log" })],
+});
+
+if (NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    })
+  );
+}
+
+//STANDARD MIDDLEWARE
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
+
+//ROUTES
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+// CATCH ANY THROWN ERRORS AND THEN DEFINE THE ERROR AND KEEP THE APPLICATION RUNNING;
+//STILL MIDDLEWARE
+app.use(function errorHandler(error, req, res, next) {
+  let response;
+  if (NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
+  } else {
+    console.error(error);
+    response = { message: error.message, error };
+  }
+  res.status(500).json(response);
+});
+
+//PIPELINE ENDS
+module.exports = app;
